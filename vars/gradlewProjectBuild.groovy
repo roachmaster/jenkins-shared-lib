f37b08fc7d1999b63db7a1ec92a45f02c8c5cc9e
Leonardo <leonardo.rocha1990@gmail.com>
2020-01-19 17:14:01 -0600

    updating vars

--- vars/gradlewProjectBuild.groovy
+++ vars/gradlewProjectBuild.groovy
@@ -1,4 +1,9 @@
-def call(Map pipelineParams) {
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def pipelineParams = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
     stage("build") {
         node {
             checkout scm
