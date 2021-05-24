2ca68dbd04dd30e391bf89d0ed14b859c0009665
roachmaster <leonardo.rocha1990@gmail.com>
2021-05-25 00:44:09 +0100

    Updating library

--- vars/buildGradleProject.groovy
+++ vars/buildGradleProject.groovy
@@ -1,7 +1,7 @@
 def call(Map config) {
-   sh "cd ${config.name}" 
-   sh "chmod +x ./gradlew"
-   sh "./gradlew clean build"
-   sh "cd ../" 
-   sh "pwd"
+    dir(config.name){
+        sh "chmod +x ./gradlew"
+        sh "./gradlew clean build"
+        sh "pwd"
+    }
 }
