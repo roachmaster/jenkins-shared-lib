68fcb8d22f8c8815d111d1c6d0414361cc542baa
roachmaster <leonardo.rocha1990@gmail.com>
2021-05-24 23:29:56 +0100

    Updating library

--- vars/setUpGradleProjects.groovy
+++ vars/setUpGradleProjects.groovy
@@ -1,6 +1,8 @@
 def call(Map config) {
     stage("Setting Up Gradle Environment") {
         node {
+           //checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: '7daee168-9f44-47b1-bdb6-a90058c89007', url: 'git@github.com:roachmaster/mars-rover-photo-collector.git']]]) 
+           checkout scm
            sh "ls" 
         }
     }/*
