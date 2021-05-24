911b1c866f36e908c3da79e92a3e422c4e52c3c6
roachmaster <leonardo.rocha1990@gmail.com>
2021-05-25 00:31:00 +0100

    Updating library

--- vars/setUpGradleProjects.groovy
+++ vars/setUpGradleProjects.groovy
@@ -1,11 +1,7 @@
 def call() {
-    stage("Setting Up Gradle Environment") {
-        node {
-           checkout scm
-           sh "ls" 
-           sh "chmod +x ./scripts/*"
-           sh script:"./scripts/setUpGradleProjects.sh", returnStatus:true
-           sh "which gradle"
-        }
-    }
+    checkout scm
+    sh "ls" 
+    sh "chmod +x ./scripts/*"
+    sh script:"./scripts/setUpGradleProjects.sh", returnStatus:true
+    sh "which gradle"
 }
