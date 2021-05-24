fe0615b8d30ee6166f32d5d5997a5992771d35b9
roachmaster <leonardo.rocha1990@gmail.com>
2021-05-24 23:50:51 +0100

    Updating library

--- vars/setUpGradleProjects.groovy
+++ vars/setUpGradleProjects.groovy
@@ -5,6 +5,7 @@ def call(Map config) {
            sh "ls" 
            sh "chmod +x ./scripts/*"
            sh script:"./scripts/setUpGradleProjects.sh", returnStatus:true
+           sh "which gradle"
         }
     }
 }
